8297aa4abf8edbe83e75913ecd4fc248791b3f75
Andrew Schorr <afs35mm@gmail.com>
2017-08-18 08:35:46 -0400

    fuck yes, got shit working with react

--- resources/assets/js/home.js
+++ resources/assets/js/home.js
@@ -58,10 +58,27 @@ class Child extends React.Component {
 }
 
 class SearchResults extends React.Component {
+    togglePhotoSelection(id) {
+        console.log(id);
+    }
     render() {
-        console.log(this.props)
+        const tagImgs = this.props.responseImgs.map((img, idx)=> {
+            const id = img.id;
+            const src = img.images.standard_resolution.url;
+            return (
+                <figure key={id} onClick={() => {this.togglePhotoSelection(id)}}>
+                    <img src={src} alt=""/>
+                </figure>
+            )
+
+        });
+        // This sucks, I'm aware
+        // https://stackoverflow.com/questions/16377972/how-to-align-left-last-row-line-in-multiple-line-flexbox
         return (
-            <div>fuck me</div>
+            <div className="results">
+                {tagImgs}
+                <p className="filler"></p><p className="filler"></p><p className="filler"></p><p className="filler"></p>
+            </div>
         )
     }
 }
